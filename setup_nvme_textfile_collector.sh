ddfaed0953006c318dd720f6df124fe23ea3c767
VladimirDiamandi <vladdiamandi@gmail.com>
2024-10-31 16:12:06 +0200

    add setup script for NVMe Textfile Collector

---
+++ setup_nvme_textfile_collector.sh
@@ -0,0 +1,8 @@
+apt update
+apt install -y python3-pip
+apt install -y nvme-cli
+pip3 install prometheus-client
+curl https://raw.githubusercontent.com/VladimirDiamandi/prometheus-monitoring-setup/main/nvme_metrics.py > /var/lib/node_exporter/nvme_metrics.py
+
+cron_job="*/2 * * * * /bin/bash -c '{ /usr/bin/python3 /var/lib/node_exporter/nvme_metrics.py > /var/lib/node_exporter/textfile_collector/nvme.prom; } || echo "" > /var/lib/node_exporter/textfile_collector/nvme.prom'"
+(crontab -l 2>/dev/null; echo "$cron_job") | crontab -
